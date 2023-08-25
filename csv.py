97c2480072394a20475ba6893557a458e316cef5
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-08-24 22:18:35 -0300

    arquivo com as funções

--- csv.py
+++ csv.py
@@ -1,3 +1,8 @@
+#No repositório do GitHub, você encontrará dois arquivos distintos. 
+# Por favor, faça as correções necessárias em ambos. Em seguida, 
+#integre-os de tal forma que o sistema seja capaz de armazenar os dados dos usuários em um formato CSV.
+
+
 def load_users_from_csv(filename):
     try:
         with open(filename, 'r') as file:
